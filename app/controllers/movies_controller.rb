9b626df241c52d04103b5960265919e15b12e979
EC2 Default User <ec2-user@ip-172-31-8-212.us-east-2.compute.internal>
2020-09-15 04:46:48 +0000

    part 2 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,7 +13,15 @@ class MoviesController < ApplicationController
   def index
     # @movies = Movie.all
     @movies = Movie.order(params[:sort_by])
+    if params[:ratings]
+      @movies = Movie.where(:rating => params[:ratings].keys).order(params[:sort_by])
+    end
     @sort_column = params[:sort_by]
+    @all_ratings = Movie.all_ratings
+    @set_ratings = params[:ratings]
+    if !@set_ratings
+      @set_ratings = Hash.new
+    end
     
   end
 
